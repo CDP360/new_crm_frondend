eb5e44420cdbd5b795b73a4102b214d30eb7089a
HARI_TUF <hari@cdp360.com>
2022-11-25 15:45:57 +0530

    changes in sidebar img

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -1,27 +1,35 @@
 import React, { useState } from "react";
 import "./sidebar.css";
-import Dashboard from "../../Assests/dashboard-active.svg";
-import Automation from "../../Assests/Automation.svg";
-import Marketing from "../../Assests/email.svg";
+import Dashboard from "../../Assests/dashboard-white.svg";
+import Automation from "../../Assests/automation-black.svg";
+import emailblack from "../../Assests/email-black.svg";
+import dashboardblack from "../../Assests/dashboard-black.svg";
+import Marketing from "../../Assests/email-white.svg";
 import { useNavigate } from "react-router-dom";
+import automationwhite from "../../Assests/automation-white.svg";
 
 function Sidebar() {
   const [step, setStep] = useState(0);
+
+  const [image, setImage] = useState(0);
 
   const navigate = useNavigate();
 
   function dashboard() {
     setStep(0);
     navigate("");
+    setImage(0);
   }
 
   function automation() {
     navigate("/automation");
     setStep(1);
+    setImage(1);
   }
   function marketing() {
     navigate("/marketing");
     setStep(2);
+    setImage(2);
   }
   return (
     <div className="sidebar-maincontainer">
@@ -33,21 +41,49 @@ function Sidebar() {
           className={`dashboard-block ${step === 0 ? "active" : "deactive"} `}
           onClick={dashboard}
         >
-          <img src={Dashboard} className="sidebar-img" />
+          {image === 0 ? (
+            <>
+              <img src={dashboardblack} className="sidebar-img" />
+            </>
+          ) : (
+            <>
+              <img src={Dashboard} className="sidebar-img" />
+            </>
+          )}
+
           <p className="sidebar-name">Dashboard</p>
         </div>
         <div
           className={`dashboard-block ${step === 1 ? "active" : "deactive"} `}
           onClick={automation}
         >
-          <img src={Automation} className="sidebar-img" />
+          {image === 1 ? (
+            <>
+              {" "}
+              <img src={Automation} className="sidebar-img" />
+            </>
+          ) : (
+            <>
+              <img src={automationwhite} className="sidebar-img" />
+            </>
+          )}
+
           <p className="sidebar-name">Automation</p>
         </div>
         <div
           className={`dashboard-block ${step === 2 ? "active" : "deactive"} `}
           onClick={marketing}
         >
-          <img src={Marketing} className="sidebar-img" />
+          {image === 2 ? (
+            <>
+              <img src={emailblack} className="sidebar-img" />
+            </>
+          ) : (
+            <>
+              <img src={Marketing} className="sidebar-img" />
+            </>
+          )}
+
           <p className="sidebar-name">Marketing</p>
         </div>
       </div>
