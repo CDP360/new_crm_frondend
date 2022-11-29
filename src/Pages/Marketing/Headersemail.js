962e9ad123dcc30c0e3a51e4ab21de2a2e1b2fe9
HARI_TUF <hari@cdp360.com>
2022-11-29 11:16:13 +0530

    changes in headers

--- src/Pages/Marketing/Headersemail.js
+++ src/Pages/Marketing/Headersemail.js
@@ -4,10 +4,13 @@ import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 import Button from "@mui/material/Button";
 import FolderOpenIcon from "@mui/icons-material/FolderOpen";
 import { useNavigate } from "react-router-dom";
+import "./headersemail.css";
 
 function Headersemail() {
   const navigate = useNavigate();
   const [headerbutton, setHeaderButton] = useState(true);
+
+  const [data, setData] = useState(0);
 
   const Campaigns = () => {
     navigate("/marketing");
@@ -20,76 +23,117 @@ function Headersemail() {
   };
   return (
     <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        padding: "1%",
+        paddingTop: "2%",
+        backgroundColor: "#dfe3de",
+      }}
+    >
+      <div
         style={{
           display: "flex",
-          justifyContent: "space-between",
-          padding: "1%",
-          paddingTop: "2%",
-          backgroundColor: "#dfe3de",
-          borderBottom: "1px solid grey",
+          width: "75%",
+          justifyContent: "space-around",
         }}
       >
-        <div
+        <p
           style={{
-            display: "flex",
-            width: "75%",
-            justifyContent: "space-around",
+            marginBottom: "auto",
+            marginTop: "auto",
           }}
         >
-          <p
-            style={{ marginBottom: "auto", marginTop: "auto" }}
-          >
-            Social Planner
-          </p>
+          Social Planner
+        </p>
+
+        <div
+          onClick={() => setData(0)}
+          className={`${data === 0 ? "border-data1" : "border-deactive"}`}
+        >
           <Dropdown>
-          <Dropdown.Toggle
-            variant="light"
-            id="dropdown-basic"
-            style={{
-              border: "none",
-              backgroundColor: "#dfe3de",
-              marginTop: "3%",
-            }}
-          >
-            Email
-          </Dropdown.Toggle>
+            <Dropdown.Toggle
+              variant="light"
+              id="dropdown-basic"
+              style={{
+                border: "none",
+                backgroundColor: "#dfe3de",
+                marginTop: "3%",
+              }}
+            >
+              Email
+            </Dropdown.Toggle>
 
-          <Dropdown.Menu>
-            <Dropdown.Item onClick={Campaigns}>Camapign</Dropdown.Item>
-            <Dropdown.Item onClick={Template}>Templates</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-          <p
-            style={{ marginBottom: "auto", marginTop: "auto" }}
-          >
-            Templates
-          </p>
-          <p style={{ marginBottom: "auto", marginTop: "auto" }}>Trigger Links</p>
-          <p style={{ marginBottom: "auto", marginTop: "auto" }}>Affiliate Manager</p>
-
+            <Dropdown.Menu>
+              <Dropdown.Item onClick={Campaigns}>Camapign</Dropdown.Item>
+              <Dropdown.Item onClick={Template}>Templates</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
         </div>
-        <div className="automation-header-right">
+
+        <p
+          style={{
+            marginBottom: "auto",
+            marginTop: "auto",
+          }}
+          onClick={() => setData(1)}
+          className={`${data === 1 ? "border-data" : "border-deactive"}`}
+        >
+          Templates
+        </p>
+        <p
+          style={{
+            marginBottom: "auto",
+            marginTop: "auto",
+          }}
+          onClick={() => setData(2)}
+          className={`${data === 2 ? "border-data" : "border-deactive"}`}
+        >
+          Trigger Links
+        </p>
+        <p
+          style={{
+            marginBottom: "auto",
+            marginTop: "auto",
+          }}
+          onClick={() => setData(3)}
+          className={`${data === 3 ? "border-data" : "border-deactive"}`}
+        >
+          Affiliate Manager
+        </p>
+      </div>
+      <div className="automation-header-right">
         {headerbutton ? (
           <>
-            <Button variant="primary" startIcon={<AddCircleOutlineIcon />} style={{ textTransform: "capitalize" }}>
+            <Button
+              variant="primary"
+              startIcon={<AddCircleOutlineIcon />}
+              style={{ textTransform: "capitalize" }}
+            >
               Campaign
             </Button>
           </>
         ) : (
           <>
-            <Button variant="primary" startIcon={<AddCircleOutlineIcon />} style={{ textTransform: "capitalize" }}>
+            <Button
+              variant="primary"
+              startIcon={<AddCircleOutlineIcon />}
+              style={{ textTransform: "capitalize" }}
+            >
               Templates
             </Button>
           </>
         )}
-        {/* <Button variant="primary" startIcon={<AddCircleOutlineIcon />}>
-          Campaign
-        </Button> */}
-        <Button variant="outlined" startIcon={<FolderOpenIcon />} style={{ textTransform: "capitalize" }}>
+
+        <Button
+          variant="outlined"
+          startIcon={<FolderOpenIcon />}
+          style={{ textTransform: "capitalize" }}
+        >
           Folder
         </Button>
-        </div>
       </div>
+    </div>
   );
 }
 
